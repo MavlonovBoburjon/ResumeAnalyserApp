d5f8ec5f660acdc55660625aba9ed601239a9acc
Bobur <mavlonovboburjon2508@gmail.com>
2024-04-26 00:03:36 +0500

    sixth commit

--- App.py
+++ App.py
@@ -81,19 +81,6 @@ st.set_page_config(
     page_icon='./Logo/Logo.ico',
 )
 
-
-def delete_files_in_directory(directory_path):
-    try:
-        files = os.listdir(directory_path)
-        for file in files:
-            file_path = os.path.join(directory_path, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        print("All files deleted successfully.")
-    except OSError:
-        print("Error occurred while deleting files.")
-
-
 def run():
     st.title("Aqlli Resume Tahlilchisi")
     img = Image.open('Logo/Logo.jpg')
@@ -336,7 +323,4 @@ def run():
         else:
             st.error('Nimadir xato..')
 
-
-directory_path = 'resume'
-delete_files_in_directory(directory_path)
 run()
