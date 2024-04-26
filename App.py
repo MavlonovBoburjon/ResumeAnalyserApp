b2acf3242166008e45827d43917a48ea21197ba3
Bobur <mavlonovboburjon2508@gmail.com>
2024-04-27 01:15:15 +0500

    sixth commit

--- App.py
+++ App.py
@@ -52,15 +52,19 @@ def pdf_reader(file):
 
 
 def show_pdf(file_path):
+    path = './example.txt'
+
+    check_file = os.path.isfile(path)
+
     with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode()
     print(file_path)
     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
     # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" type="application/pdf"></iframe>'
 
-    st.markdown(file_path, unsafe_allow_html=True)
-    st.markdown('<iframe src="data:application/pdf;base64,{}" width="100%" height="800px" '
-                'type="application/pdf"></iframe>'.format(base64_pdf), unsafe_allow_html=True)
+    st.markdown(check_file, unsafe_allow_html=True)
+    st.markdown(f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" '
+                'type="application/pdf"></iframe>', unsafe_allow_html=True)
 
 
 def course_recommender(course_list):
